f2e25beabd3508311da3e6077840682ba7b56275
Kai Grossjohann <kai.grossjohann@epochtimes.de>
2015-08-24 21:41:47 +0200

    Print a usage message.

--- src/sircl/core.clj
+++ src/sircl/core.clj
@@ -9,7 +9,8 @@ First arg is index file to create, search arg is directory to index."
   [index directory]
   (printf "Indexing directory %s writing to file %s\n"
           directory index)
-  (index/index-directory directory index))
+  (index/index-directory directory index)
+  (println "All done."))
 
 (defn search-collection
   "Search an on-disk index by some terms."
@@ -18,6 +19,16 @@ First arg is index file to create, search arg is directory to index."
   (let [r (apply search/search index terms)]
     (doall (map (fn [x] (printf "%f\t%s\n" (float (second x)) (first x))) r))
     (println "All done.")))
+
+(defn help
+  "Print usage instructions."
+  []
+  (println "Usage: java -jar ... index INDEX DIRECTORY")
+  (println "       java -jar ... search INDEX TERM...")
+  (println)
+  (println "INDEX is the name of an index file.")
+  (println "DIRECTORY is the name of a directory containing text files.")
+  (println "TERM is a search term."))
 
 (defn -main
   "Index a collection of documents or search in an index."
@@ -28,4 +39,6 @@ First arg is index file to create, search arg is directory to index."
       (= cmd "index")
       (apply index-collection (rest args))
       (= cmd "search")
-      (apply search-collection (rest args)))))
+      (apply search-collection (rest args))
+      :else
+      (help))))
